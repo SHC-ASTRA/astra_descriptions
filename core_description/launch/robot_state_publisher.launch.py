7120a03274ae55a1ce9487ef58751836eafb9469
ryleu <69326171+ryleu@users.noreply.github.com>
2025-10-23 03:48:34 -0500

    format files and update licenses

--- core_description/launch/robot_state_publisher.launch.py
+++ core_description/launch/robot_state_publisher.launch.py
@@ -10,56 +10,67 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
 
     # Set the path to this package.
-    pkg_share = FindPackageShare(package='core_description').find('core_description')
+    pkg_share = FindPackageShare(package="core_description").find("core_description")
 
     # Launch configuration variables specific to simulation
-    urdf_model = LaunchConfiguration('urdf_model')
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_rviz = LaunchConfiguration('use_rviz')
-    use_sim_time = LaunchConfiguration('use_sim_time')
+    urdf_model = LaunchConfiguration("urdf_model")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
+    use_rviz = LaunchConfiguration("use_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
     # Declare the launch arguments
     declare_urdf_model_path_cmd = DeclareLaunchArgument(
-        name='urdf_model',
-        default_value=os.path.join(pkg_share, 'urdf', 'core_description.xacro'),
-        description='Absolute path to robot urdf file')
+        name="urdf_model",
+        default_value=os.path.join(pkg_share, "urdf", "core_description.xacro"),
+        description="Absolute path to robot urdf file",
+    )
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
-        name='rviz_config_file',
-        default_value=os.path.join(pkg_share, 'config', 'rviz_basic_settings.rviz'),
-        description='Full path to the RVIZ config file to use')
+        name="rviz_config_file",
+        default_value=os.path.join(pkg_share, "config", "rviz_basic_settings.rviz"),
+        description="Full path to the RVIZ config file to use",
+    )
 
     declare_use_rviz_cmd = DeclareLaunchArgument(
-        name='use_rviz',
-        default_value='True',
-        description='Whether to start RVIZ')
+        name="use_rviz", default_value="True", description="Whether to start RVIZ"
+    )
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
-        name='use_sim_time',
-        default_value='True',
-        description='Use simulation (Gazebo) clock if true')
+        name="use_sim_time",
+        default_value="True",
+        description="Use simulation (Gazebo) clock if true",
+    )
 
     # Specify the actions
 
     # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
     start_robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'use_sim_time': use_sim_time,
-                    'robot_description': ParameterValue(Command(['xacro ', urdf_model]), value_type=str)}],
-        arguments=[urdf_model])
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[
+            {
+                "use_sim_time": use_sim_time,
+                "robot_description": ParameterValue(
+                    Command(["xacro ", urdf_model]), value_type=str
+                ),
+            }
+        ],
+        arguments=[urdf_model],
+    )
 
     # Launch RViz
     start_rviz_cmd = Node(
         condition=IfCondition(use_rviz),
-        package='rviz2',
-        executable='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config_file],
-        parameters=[{'use_sim_time': use_sim_time}])
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file],
+        parameters=[{"use_sim_time": use_sim_time}],
+    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
