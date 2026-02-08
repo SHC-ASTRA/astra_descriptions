8354f9f36988cfac30f90c98037e2ea9c00a0002
David <ds0196@uah.edu>
2026-02-08 17:27:46 -0600

    feat: (core) add controller manager to spawn_controllers launch

--- core_description/launch/spawn_controllers.launch.py
+++ core_description/launch/spawn_controllers.launch.py
@@ -5,12 +5,13 @@ from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
 )
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, EqualsSubstitution
+from launch.substitutions import LaunchConfiguration, EqualsSubstitution, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
 
 from srdfdom.srdf import SRDF
 
@@ -24,6 +25,21 @@ def generate_launch_description():
             name="hardware_mode",
             default_value="gazebo",
             description="Hardware mode: 'preview' for URDF preview, 'gazebo' for simulation, 'physical' for real hardware",
+        )
+    )
+
+    # Controller Manager
+    ld.add_action(
+        Node(
+            condition=UnlessCondition(EqualsSubstitution(LaunchConfiguration("hardware_mode"), "gazebo")),
+            package="controller_manager",
+            executable="ros2_control_node",
+            parameters=[
+                PathJoinSubstitution([FindPackageShare("core_description"), "config", "ros2_controllers.yaml"]),
+            ],
+            remappings=[
+                ("/controller_manager/robot_description", "/robot_description"),
+            ],
         )
     )
 
