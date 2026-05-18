e80df214047efe7913a75cc1b58365e1dbddda38
David <ds0196@uah.edu>
2026-05-18 16:24:05 -0500

    feat(docker): add Wayland support to run script

--- gazebo/run_container.sh
+++ gazebo/run_container.sh
@@ -11,11 +11,17 @@ echo "Running the Gazebo container."
 echo "astra_descriptions will be volume mounted to /home/ubuntu/ros2_ws/src/astra_descriptions."
 echo "Try running: source install/setup.bash && ros2 launch core_gazebo core.gazebo.launch.py"
 
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+  echo "Using Wayland for display."
+  SESSION_VARS="-e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY"
+else
+  echo "Using X11 for display."
+  SESSION_VARS="-e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix"
+fi
+
 docker run -it --rm \
   --net=host \
-  --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" \
   --env="FASTDDS_BUILTIN_TRANSPORTS=UDPv4" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="$SCRIPT_DIR/..:/home/ubuntu/ros2_ws/src/astra_descriptions:rw" \
+  $SESSION_VARS \
   astra/gazebo:latest
